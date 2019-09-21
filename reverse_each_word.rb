889d9b07b6c46523b0b03fdbaf367515dff7461b
Nick <nick.moi@gmail.com>
2019-09-21 12:30:20 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -5,6 +5,6 @@ def reverse_each_word(string)
   #  reversedStr.concat(n.reverse, " ")
   #end
   #reversedStr[0, reversedStr.length - 1]
-  strArr.collect { |n| n.reverse + " " }
+  reversedStr.concat(strArr.collect { |n| n.reverse + " " })
   reversedStr[0, reversedStr.length - 1]
 end
